3a7734a4d262e155a1a26171604075b7fbcaf1d4
Tram Khoi Nguyen <112838098+reisohz@users.noreply.github.com>
2022-11-17 14:25:20 +0700

    Update buoi6_baitap2.cpp

--- buoi6_baitap2.cpp
+++ buoi6_baitap2.cpp
@@ -2,8 +2,33 @@
 #include <conio.h>
 #include <string.h>
 #include<stdlib.h>
+
+struct SinhVien
+{
+	char Masv[10];
+	char Hotensv[30];
+	char Phai[10];
+	int Namsinh;
+	char Nganhhoc[30];
+	float Kqcuoikhoa;
+	char Xeploai[20];
+	char Quequan[30];
+	
+};
+
+typedef struct SinhVien SINHVIEN;
+void TIMSV(SINHVIEN a[], int n);
+void INGX(SINHVIEN a[], int n);
+void Sapxep(SINHVIEN a[], int n);
+void Nhapsv(SINHVIEN &x);
+void NhapsvN(SINHVIEN a[], int n);
+void Xuatsv(SINHVIEN);
+void XuatsvN(SINHVIEN a[], int n);
+
+
 int main()
 { int n, key;
+SINHVIEN x, a[10];
 while(true)
 {
 system("cls");
@@ -30,6 +55,7 @@ case 1:
 do{
 printf("\nNHAP SO LUONG SINH VIEN: "); scanf("%d", &n);
 }while(n <= 0);
+NhapsvN(a,n);
 
  
 
@@ -40,24 +66,30 @@ break;
 
 case 2:
 //go%i hom thuc de hien cong viec
+XuatsvN(a,n);
 printf("\nBam phim bat ky de tiep tuc!");
 getch();
 break;
 
 case 3:
-printf("\nBam phim bat ky de tiep tuc!");
 //goi ham thuc de hien cong viec
+Sapxep(a,n);
+XuatsvN(a,n);
+printf("\nBam phim bat ky de tiep tuc!");
 getch();
 break;
 
 case 4:
-printf("\nBam phim bat ky de tiep tuc");
 //goi ham thuc de hien cong viec
+INGX(a,n);
+printf("\nBam phim bat ky de tiep tuc");
+getch();
 break;
 
 case 5:
-printf("\nBam phim bat ky de tiep tuc");
 //goi ham thuc de hien cong viec
+TIMSV(a,n);
+printf("\nBam phim bat ky de tiep tuc");
 getch();
 break;
 
@@ -72,7 +104,128 @@ break;
 }
 }
 }
- 
+void Nhapsv(SINHVIEN &x)
+{
+	fflush(stdin);
+	printf("\nNhap ma sv: ");
+	gets(x.Masv);
+	
+	//scanf("%d",&x.Masv);
+	fflush(stdin);
+  printf("\nNhap ho ten: ");
+  gets(x.Hotensv);
+    fflush(stdin);
+  printf("\nNhap phai: ");
+  gets(x.Phai);
+  fflush(stdin);
+  printf("\nNhap nganh hoc: ");
+  gets(x.Nganhhoc);
+  fflush(stdin);
+  printf("\nNhap que quan: ");
+  gets(x.Quequan);
+  
+  
+  do
+  {
+  	printf("\nNhap nam sinh: ");
+  	scanf("%d",&x.Namsinh);
+  	
+  }while(x.Namsinh<1980 || x.Namsinh>2010);
+  
+  do
+  {
+  	printf("\nNhap KQ cuoi khoa: ");
+  	scanf("%f",&x.Kqcuoikhoa);
 
- 
-
+  }while(x.Kqcuoikhoa<0||x.Kqcuoikhoa>4);
+  
+  
+  fflush(stdin);
+  if(x.Kqcuoikhoa<=1.5)
+  {strcpy(x.Xeploai,"Yeu");}
+  else
+    if(x.Kqcuoikhoa<=2.5)
+  {strcpy(x.Xeploai,"Trung binh");}
+  else
+  if(x.Kqcuoikhoa<=3.0)
+  {strcpy(x.Xeploai,"Kha");}
+  else
+    if(x.Kqcuoikhoa<=3.8)
+    {strcpy(x.Xeploai,"Gioi");}
+  else
+      {strcpy(x.Xeploai,"Xuat sac");}
+
+  
+}
+void NhapsvN(SINHVIEN a[], int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		printf("\n Nhap sinh vien thu %d",i+1);
+		Nhapsv(a[i]);
+	}
+}
+void Xuatsv(SINHVIEN x)
+{
+	printf("|%5s|%-18s|%-6s|%10d|%-11s|%12.1f|%-10s|%-10s|",x. Masv,x.Hotensv,x.Phai, x.Namsinh,x.Nganhhoc,x.Kqcuoikhoa,x.Xeploai,x.Quequan);
+	
+}
+
+void XuatsvN(SINHVIEN a[],int n)
+{system("cls");
+printf("\n\n|-------------------------------------------------------------------------------------------\n");
+     printf("|Ma SV| Ho ten sinh vien | Phai | Nam sinh | Nganh hoc |KQ cuoi khoa| Xep loai | Que quan |");
+
+for(int i=0;i<n;i++)
+{
+   printf("\n|-----|------------------|------|----------|-----------|------------|----------|----------|\n");
+Xuatsv(a[i]);
+}
+}
+void Sapxep(SINHVIEN a[], int n)
+{ SINHVIEN tmp;
+for(int i = 0;i<n;++i){
+	for(int j= i+1; j < n;++j){
+		if(a[i].Kqcuoikhoa> a[j].Kqcuoikhoa){
+			tmp = a[i];
+			a[i]=a[j];
+			a[j] = tmp;
+		}
+	}
+}
+}
+
+void INGX(SINHVIEN a[], int n)
+{system("cls");
+printf("\n\n|-------------------------------------------------------------------------------------------\n");
+     printf("|Ma SV| Ho ten sinh vien | Phai | Nam sinh | Nganh hoc |KQ cuoi khoa| Xep loai | Que quan |");
+
+for(int i=0;i<n;i++)
+{ if(a[i].Kqcuoikhoa>=3.0)
+    {
+   printf("\n|-----|------------------|------|----------|-----------|------------|----------|----------|\n");
+    Xuatsv(a[i]);
+    }
+}
+
+
+}
+
+void TIMSV(SINHVIEN a[], int n)
+{system("cls");
+fflush(stdin);
+char Matim[10];
+printf("\n Nhap ma sinh vien can tim: ");
+gets(Matim);
+
+printf("\n\n|-------------------------------------------------------------------------------------------\n");
+     printf("|Ma SV| Ho ten sinh vien | Phai | Nam sinh | Nganh hoc |KQ cuoi khoa| Xep loai | Que quan |");
+     
+     for(int i=0;i<n;i++)
+     { if(strcmp(a[i].Masv,Matim)==0)
+        {
+   printf("\n|-----|------------------|------|----------|-----------|------------|----------|----------|\n");  
+   Xuatsv(a[i]);      	
+        }
+     }
+}
